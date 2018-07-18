bb886d0225e5f61162df01610d8e66e0bea0e936
finn <finnr@protonmail.com>
2018-07-18 12:32:59 -0700

    no longer interacts with other bots

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import config
 import requests
 
 #logging
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 
-bot = commands.AutoShardedBot(command_prefix='+')
+bot = commands.AutoShardedBot(command_prefix='!')
 
 #Removes default help command
 bot.remove_command("help")
@@ -80,60 +80,77 @@ async def upvoters(ctx):
 #Sends analytics to a channel in the Poll Bot server
 @bot.command(name='!stats', pass_context=True)
 async def srvrs(ctx):
-    chnl = bot.get_channel(396183943910522880)
-    srvr = bot.get_guild(300487910619217921)
-    bot.app = await bot.application_info()
-    guilds = 'guilds: ' + str(len(bot.guilds))
-    users = 'users: ' + str(len(list(bot.get_all_members())))
-    uniques = 'unique users: ' + str(len(discord.utils._unique(bot.get_all_members())))
-    await chnl.send(guilds)
-    await chnl.send(users)
-    await chnl.send(uniques)
-    await chnl.send("server members: " + str(srvr.member_count))
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        chnl = bot.get_channel(396183943910522880)
+        srvr = bot.get_guild(300487910619217921)
+        bot.app = await bot.application_info()
+        guilds = 'guilds: ' + str(len(bot.guilds))
+        users = 'users: ' + str(len(list(bot.get_all_members())))
+        uniques = 'unique users: ' + str(len(discord.utils._unique(bot.get_all_members())))
+        await chnl.send(guilds)
+        await chnl.send(users)
+        await chnl.send(uniques)
+        await chnl.send("server members: " + str(srvr.member_count))
 
 #Help Command
 @bot.command(name='help', pass_context=True)
 async def hlp(ctx):
-    emb1 = discord.Embed(description="**Reaction Poll**\nCreate a reaction poll by typing '+poll *your message*’. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\n\n**Strawpoll**\nCreate a strawpoll by typing '+strawpoll {title} [Option1] [Option2] [Option 3]', with up to 30 options.\n\n**Other Commands**\n+updates, +invite, +donate\n\n**Still Have Questions?**\nJoin our official discord server: <https://discord.gg/FhT6nUn>" + "\n" + "Ask us on twitter: <https://twitter.com/DiscordPollBot>", colour=0x83bae3)
-    try:
-        await ctx.author.send(embed=emb1)
-        await ctx.message.channel.send('Check your DMs!')
-    except discord.HTTPException:
-        await ctx.message.channel.send(embed=emb1)
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        emb1 = discord.Embed(description="**Reaction Poll**\nCreate a reaction poll by typing '+poll *your message*’. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\n\n**Strawpoll**\nCreate a strawpoll by typing '+strawpoll {title} [Option1] [Option2] [Option 3]', with up to 30 options.\n\n**Other Commands**\n+updates, +invite, +donate\n\n**Still Have Questions?**\nJoin our official discord server: <https://discord.gg/FhT6nUn>" + "\n" + "Ask us on twitter: <https://twitter.com/DiscordPollBot>", colour=0x83bae3)
+        try:
+            await ctx.author.send(embed=emb1)
+            await ctx.message.channel.send('Check your DMs!')
+        except discord.HTTPException:
+            await ctx.message.channel.send(embed=emb1)
 
 @bot.command(name='donate', pass_context=True)
 async def hlp(ctx):
-    emb1 = discord.Embed(title='Support', description="We currently don't need donations, but it would be nice if you could upvote Poll Bot here: https://discordbots.org/bot/298673420181438465/vote (and get the upvoter role on the Poll Bot discord server: https://discord.gg/FhT6nUn)", color=0x83bae3)
-    await ctx.message.channel.send(embed=emb1)
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        emb1 = discord.Embed(title='Support', description="We currently don't need donations, but it would be nice if you could upvote Poll Bot here: https://discordbots.org/bot/298673420181438465/vote (and get the upvoter role on the Poll Bot discord server: https://discord.gg/FhT6nUn)", color=0x83bae3)
+        await ctx.message.channel.send(embed=emb1)
 
 #Invite link (if people want to add Poll Bot to their own server)
 @bot.command(name='invite', pass_context=True)
 async def info(ctx):
-    emb1 = discord.Embed(description='Invite Poll Bot to your server: <https://discordapp.com/oauth2/authorize?client_id=298673420181438465&scope=bot&permissions=0>', colour=0x83bae3)
-    await ctx.message.channel.send(embed=emb1)
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        emb1 = discord.Embed(description='Invite Poll Bot to your server: <https://discordapp.com/oauth2/authorize?client_id=298673420181438465&scope=bot&permissions=0>', colour=0x83bae3)
+        await ctx.message.channel.send(embed=emb1)
 
 #Where the latest updates get put
 @bot.command(name='updates', pass_context=True)
 async def updts(ctx):
-    emb1 = discord.Embed(description="**Please join the Poll Bot server and see #announcements to see the latest updates! <https://discord.gg/FhT6nUn>**\n\n_**Don't forget you can use all of Poll Bot's features within direct messages. Just open a direct message with Poll Bot and use it like you would normally.**_")
-
-    await ctx.message.channel.send(embed=emb1)
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        emb1 = discord.Embed(description="**Please join the Poll Bot server and see #announcements to see the latest updates! <https://discord.gg/FhT6nUn>**\n\n_**Don't forget you can use all of Poll Bot's features within direct messages. Just open a direct message with Poll Bot and use it like you would normally.**_")
+        await ctx.message.channel.send(embed=emb1)
 
 #Reaction Poll
 @bot.command(name="poll", pass_context=True)
 async def poll(ctx):
-    await ctx.message.add_reaction('👍')
-    await ctx.message.add_reaction('👎')
-    await ctx.message.add_reaction('🤷')
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        await ctx.message.add_reaction('👍')
+        await ctx.message.add_reaction('👎')
+        await ctx.message.add_reaction('🤷')
 
 #Reaction Poll with 2 options
 @bot.command(name="poll2", pass_context=True)
 async def poll(ctx):
-    await ctx.message.add_reaction('👍')
-    await ctx.message.add_reaction('👎')
-
-@bot.command(name="retrying", pass_context=True)
-
+    if ctx.message.author.bot == True:
+        print('bot tried to send message and was denied')
+    else:
+        await ctx.message.add_reaction('👍')
+        await ctx.message.add_reaction('👎')
 
 #Strawpoll
 @bot.event
@@ -142,7 +159,10 @@ async def on_message(message):
     messageContent = message.content
     if message.content.startswith(command_name):
         pollURL = await createStrawpoll(messageContent)
-        await message.channel.send(pollURL)
+        if message.author.bot == True:
+            print('bot tried to send message and was denied')
+        else:
+            await message.channel.send(pollURL)
     else:
         await bot.process_commands(message)
 
