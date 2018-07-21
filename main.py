5420aefa80a15f589f329707c3bf2404c2eaf398
finn <finnr@protonmail.com>
2018-07-21 12:17:51 -0700

    moved away from using requests for strawpolls

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import config
 import requests
 
 #logging
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 
-bot = commands.AutoShardedBot(command_prefix='!')
+bot = commands.AutoShardedBot(command_prefix='+')
 
 #Removes default help command
 bot.remove_command("help")
@@ -39,12 +39,10 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('--------')
-    bot.app = await bot.application_info()
-    owner = bot.app.owner
+    """
     while True:
         chnl = bot.get_channel(396183943910522880)
         srvr = bot.get_guild(300487910619217921)
-        bot.app = await bot.application_info()
         guilds = 'guilds: ' + str(len(bot.guilds))
         users = 'users: ' + str(len(list(bot.get_all_members())))
         uniques = 'unique users: ' + str(len(discord.utils._unique(bot.get_all_members())))
@@ -53,7 +51,7 @@ async def on_ready():
         await chnl.send(uniques)
         await chnl.send("server members: " + str(srvr.member_count))
         await asyncio.sleep(86400)
-
+    """
 @bot.event
 async def on_guild_join(guild):
     print("New guild!")
@@ -80,12 +78,11 @@ async def upvoters(ctx):
 #Sends analytics to a channel in the Poll Bot server
 @bot.command(name='!stats', pass_context=True)
 async def srvrs(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         chnl = bot.get_channel(396183943910522880)
         srvr = bot.get_guild(300487910619217921)
-        bot.app = await bot.application_info()
         guilds = 'guilds: ' + str(len(bot.guilds))
         users = 'users: ' + str(len(list(bot.get_all_members())))
         uniques = 'unique users: ' + str(len(discord.utils._unique(bot.get_all_members())))
@@ -97,7 +94,7 @@ async def srvrs(ctx):
 #Help Command
 @bot.command(name='help', pass_context=True)
 async def hlp(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         emb1 = discord.Embed(description="**Reaction Poll**\nCreate a reaction poll by typing '+poll *your message*’. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\n\n**Strawpoll**\nCreate a strawpoll by typing '+strawpoll {title} [Option1] [Option2] [Option 3]', with up to 30 options.\n\n**Other Commands**\n+updates, +invite, +donate\n\n**Still Have Questions?**\nJoin our official discord server: <https://discord.gg/FhT6nUn>" + "\n" + "Ask us on twitter: <https://twitter.com/DiscordPollBot>", colour=0x83bae3)
@@ -109,7 +106,7 @@ async def hlp(ctx):
 
 @bot.command(name='donate', pass_context=True)
 async def hlp(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         emb1 = discord.Embed(title='Support', description="We currently don't need donations, but it would be nice if you could upvote Poll Bot here: https://discordbots.org/bot/298673420181438465/vote (and get the upvoter role on the Poll Bot discord server: https://discord.gg/FhT6nUn)", color=0x83bae3)
@@ -118,7 +115,7 @@ async def hlp(ctx):
 #Invite link (if people want to add Poll Bot to their own server)
 @bot.command(name='invite', pass_context=True)
 async def info(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         emb1 = discord.Embed(description='Invite Poll Bot to your server: <https://discordapp.com/oauth2/authorize?client_id=298673420181438465&scope=bot&permissions=0>', colour=0x83bae3)
@@ -127,7 +124,7 @@ async def info(ctx):
 #Where the latest updates get put
 @bot.command(name='updates', pass_context=True)
 async def updts(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         emb1 = discord.Embed(description="**Please join the Poll Bot server and see #announcements to see the latest updates! <https://discord.gg/FhT6nUn>**\n\n_**Don't forget you can use all of Poll Bot's features within direct messages. Just open a direct message with Poll Bot and use it like you would normally.**_")
@@ -136,7 +133,7 @@ async def updts(ctx):
 #Reaction Poll
 @bot.command(name="poll", pass_context=True)
 async def poll(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.author.bot:
         print('bot tried to send message and was denied')
     else:
         await ctx.message.add_reaction('👍')
@@ -146,7 +143,7 @@ async def poll(ctx):
 #Reaction Poll with 2 options
 @bot.command(name="poll2", pass_context=True)
 async def poll(ctx):
-    if ctx.message.author.bot == True:
+    if ctx.message.author.bot:
         print('bot tried to send message and was denied')
     else:
         await ctx.message.add_reaction('👍')
@@ -159,7 +156,7 @@ async def on_message(message):
     messageContent = message.content
     if message.content.startswith(command_name):
         pollURL = await createStrawpoll(messageContent)
-        if message.author.bot == True:
+        if message.author.bot:
             print('bot tried to send message and was denied')
         else:
             await message.channel.send(pollURL)
@@ -197,19 +194,18 @@ async def createStrawpoll(message):
                 second1 = second + 1
                 option.append(newMessage[first:second])
                 loopTime+=1
-    strawpollAPI = strawpoll.API()
-    try:
-        r = requests.post('https://www.strawpoll.me/api/v2/polls', json = {"title": title, "options": option[:(len(option)-1)], "multi": "true"}, headers={"Content Type": "application/json"})
-        json = r.json()
-        return "https://strawpoll.me/" + str(json["id"])
 
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://www.strawpoll.me/api/v2/polls', json = {"title": "a", "options": option[:(len(option)-1)], "multi": "false"}, headers={"Content Type": "application/json"}) as resp:
+                json = await resp.json()
+                return "https://strawpoll.me/" + str(json["id"])
 
     except strawpoll.errors.HTTPException:
         return "Please make sure you are using the format '+strawpoll {title} [Option1] [Option2] [Option 3]'"
 
     except KeyError:
         return "Please make sure you are using the format '+strawpoll {title} [Option1] [Option2] [Option 3]'"
-
 
 if __name__ == '__main__':
     bot.run(config.discordToken)
