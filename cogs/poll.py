1bf23cb627854fc7a5cf5c3dde339f91bf99ea54
finn <finnr@protonmail.com>
2018-08-16 22:10:58 -0700

    fixed a few bugs and added support for poll:

--- cogs/poll.py
+++ cogs/poll.py
@@ -20,72 +20,76 @@ class Poll:
                     "\N{REGIONAL INDICATOR SYMBOL LETTER W}", "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
                     "\N{REGIONAL INDICATOR SYMBOL LETTER Y}", "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"]
 
-    @commands.command(name="poll")
-    async def poll(self, ctx):
-        if not ctx.message.author.bot:
-            message = ctx.message.content
-            if message.find("{") == -1:
-                print(message.find("{"))
-                await ctx.message.add_reaction('👍')
-                await ctx.message.add_reaction('👎')
-                await ctx.message.add_reaction('🤷')
+    async def on_message(self, message):
+        if not message.author.bot:
+            if message.content.startswith("+poll") or message.content.startswith("poll:") or message.content.startswith("Poll") or message.content.startswith("POLL") or message.content.startswith("+poll:" or message.content.startswith("+Poll:") or message.content.startswith("+POLL:")):
+                messageContent = message.content
+                if messageContent.find("{") == -1:
+                    await message.add_reaction('👍')
+                    await message.add_reaction('👎')
+                    await message.add_reaction('🤷')
+                else:
+                    first = messageContent.find("{") + 1
+                    second = messageContent.find("}")
+                    title = messageContent[first:second]
+
+                    #gets the # of options and assigns them to an array
+                    newMessage = messageContent[second:]
+                    loopTime = 0
+
+                    option = []
+                    for options in messageContent:
+                        #get from } [option 1]
+                        #if newThis == -1:
+                        stillOptions = newMessage.find("[")
+                        if stillOptions != -1:
+                            if loopTime == 0:
+                                first = newMessage.find("[") + 1
+                                if newMessage.find("]") == -1:
+                                    message.channel.send("Please make sure you are using the format poll: \{title\} [option1] [option2] [option3]")
+                                else:
+                                    second = newMessage.find("]")
+                                    second1 = second + 1
+                                    option.append(newMessage[first:second])
+
+                                    loopTime+=1
+                            else:
+                                newMessage = newMessage[second1:]
+                                first = newMessage.find("[") + 1
+                                if newMessage.find("]") == -1:
+                                    message.channel.send("Please make sure you are using the format poll: \{title\} [option1] [option2] [option3]")
+                                else:
+                                    second = newMessage.find("]")
+                                    second1 = second + 1
+                                    option.append(newMessage[first:second])
+                                    loopTime+=1
+
+                    try:
+                        pollMessage = ""
+
+                        #there is probably a better way to do this
+                        i = 0
+                        for choice in option:
+                            if len(option) > 26:
+                                await message.channel.send("Maximum of 26 options")
+                                return
+                            elif not i==len(option)-1:
+                                pollMessage = pollMessage + "\n\n" + self.emojiLetters[i] + " " + choice
+                            i+=1
+
+
+                        e=discord.Embed(title="**"+title+"**", description=pollMessage + "\n\n[Support the development of Poll Bot](https://patreon.com/pollbot)", colour=0x83bae3)
+                        pollMessage = await message.channel.send(embed = e)
+                        i = 0
+                        for choice in option:
+                            if not i==len(option)-1:
+                                await pollMessage.add_reaction(self.emojiLetters[i])
+                            i+=1
+
+                    except KeyError:
+                        return "Please make sure you are using the format 'poll: {title} [Option1] [Option2] [Option 3]'"
             else:
-                first = message.find("{") + 1
-                second = message.find("}")
-                title = message[first:second]
-
-                #gets the # of options and assigns them to an array
-                newMessage = message[second:]
-                loopTime = 0
-
-                option = []
-                for options in message:
-                    #get from } [option 1]
-                    #if newThis == -1:
-                    stillOptions = newMessage.find("[")
-                    if stillOptions != -1:
-                        if loopTime == 0:
-                            first = newMessage.find("[") + 1
-
-                            second = newMessage.find("]")
-                            second1 = second + 1
-                            option.append(newMessage[first:second])
-
-                            loopTime+=1
-                        else:
-                            newMessage = newMessage[second1:]
-                            first = newMessage.find("[") + 1
-                            second = newMessage.find("]")
-                            second1 = second + 1
-                            option.append(newMessage[first:second])
-                            loopTime+=1
-
-                try:
-                    pollMessage = ""
-
-                    #there is probably a better way to do this
-                    i = 0
-                    for choice in option:
-                        if len(option) > 26:
-                            await ctx.message.channel.send("Maximum of 26 options")
-                            return
-                        elif not i==len(option)-1:
-                            pollMessage = pollMessage + "\n\n" + self.emojiLetters[i] + " " + choice
-                        i+=1
-
-
-                    e=discord.Embed(title="**"+title+"**", description=pollMessage + "\n\n[Support the development of Poll Bot](https://patreon.com/pollbot)", colour=0x83bae3)
-                    pollMessage = await ctx.message.channel.send(embed = e)
-                    i = 0
-                    for choice in option:
-                        if not i==len(option)-1:
-                            await pollMessage.add_reaction(self.emojiLetters[i])
-                        i+=1
-
-
-                #fix the message here
-                except KeyError:
-                    return "Please make sure you are using the format '+strawpoll {title} [Option1] [Option2] [Option 3]'"
+                return
 
 def setup(bot):
     bot.add_cog(Poll(bot))
