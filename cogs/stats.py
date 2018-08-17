d22abeb7d984ec0548c6eebc8e93bf2c06dc05a4
finn <finnr@protonmail.com>
2018-08-17 14:06:22 -0700

    fixed stats

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,31 +1,34 @@
 import discord
 from discord.ext import commands
 import config
+import asyncio
+import aiohttp
+import config
 
 class Stats:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_guild_join(self, guild):
-        print("a")
-        #await send_stats()
-
-    async def on_guild_remove(self, guild):
-        print("Guild left :(")
-        #await send_stats()
-        print("Stats updated.")
-
-    async def send_stats():
+    async def send_stats(self):
         tokens = (
                     ("https://discordbots.org/api/bots/%s/stats", config.orgtoken),
                     ("https://bots.discord.pw/api/bots/%s/stats", config.pwtoken),
                     ("https://botsfordiscord.com/api/v1/bots/%s", config.botsfordiscordtoken)
                 )
 
-        payload = {"Content-Type": "application/json", "server_count": len(bot.guilds)}
+        payload = {"Content-Type": "application/json", "server_count": len(self.bot.guilds)}
         for url, token in tokens:
             headers = {"Authorization": token}
-            await self.bot.http_session.post(url % bot.user.id, json=payload, headers=headers)
+            await self.bot.http_session.post(url % self.bot.user.id, json=payload, headers=headers)
+
+
+    async def on_guild_join(self, guild):
+        await self.send_stats()
+
+    async def on_guild_remove(self, guild):
+        await self.send_stats()
+
+
 
 
 
