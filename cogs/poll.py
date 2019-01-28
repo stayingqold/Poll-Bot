c1df819ec335cf53ad352e72286baac04194ad94
finn <finnr@protonmail.com>
2019-01-28 08:43:49 -0800

    no longer looking for advertisers

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,5 +1,7 @@
 import discord
 import asyncio
+from cogs.utils.Database import Database
+from datetime import datetime, timedelta
 
 
 
@@ -7,20 +9,22 @@ class Poll:
     def __init__(self, bot):
         self.bot = bot
 
-    # there is probably a better way to do this
-    emojiLetters = ["\N{REGIONAL INDICATOR SYMBOL LETTER A}", "\N{REGIONAL INDICATOR SYMBOL LETTER B}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER C}", "\N{REGIONAL INDICATOR SYMBOL LETTER D}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER E}", "\N{REGIONAL INDICATOR SYMBOL LETTER F}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER G}", "\N{REGIONAL INDICATOR SYMBOL LETTER H}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER I}", "\N{REGIONAL INDICATOR SYMBOL LETTER J}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER K}", "\N{REGIONAL INDICATOR SYMBOL LETTER L}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER M}", "\N{REGIONAL INDICATOR SYMBOL LETTER N}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER O}", "\N{REGIONAL INDICATOR SYMBOL LETTER P}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER Q}", "\N{REGIONAL INDICATOR SYMBOL LETTER R}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER S}", "\N{REGIONAL INDICATOR SYMBOL LETTER T}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER U}", "\N{REGIONAL INDICATOR SYMBOL LETTER V}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER W}", "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
-                    "\N{REGIONAL INDICATOR SYMBOL LETTER Y}", "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"]
+        self.database = Database("sample_db", "finn", "localhost")
+
+        # there is probably a better way to do this
+        self.emojiLetters = ["\N{REGIONAL INDICATOR SYMBOL LETTER A}", "\N{REGIONAL INDICATOR SYMBOL LETTER B}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER C}", "\N{REGIONAL INDICATOR SYMBOL LETTER D}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER E}", "\N{REGIONAL INDICATOR SYMBOL LETTER F}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER G}", "\N{REGIONAL INDICATOR SYMBOL LETTER H}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER I}", "\N{REGIONAL INDICATOR SYMBOL LETTER J}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER K}", "\N{REGIONAL INDICATOR SYMBOL LETTER L}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER M}", "\N{REGIONAL INDICATOR SYMBOL LETTER N}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER O}", "\N{REGIONAL INDICATOR SYMBOL LETTER P}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER Q}", "\N{REGIONAL INDICATOR SYMBOL LETTER R}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER S}", "\N{REGIONAL INDICATOR SYMBOL LETTER T}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER U}", "\N{REGIONAL INDICATOR SYMBOL LETTER V}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER W}", "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
+                        "\N{REGIONAL INDICATOR SYMBOL LETTER Y}", "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"]
 
     async def on_message(self, message):
         if not message.author.bot:
@@ -74,7 +78,7 @@ class Poll:
                             i += 1
 
                         e = discord.Embed(title="**" + title + "**",
-                                description=pollMessage + "\n\n[Want to advertise your server or product here? Email finnr@protonmail.com](mailto:finnr@protonmail.com)",
+                                description=pollMessage,
                                           colour=0x83bae3)
                         pollMessage = await message.channel.send(embed=e)
                         i = 0
@@ -84,12 +88,47 @@ class Poll:
                                 final_options.append(choice)
                                 await pollMessage.add_reaction(self.emojiLetters[i])
                             i += 1
+                        if "+duration" in message.content:
+                            messageWords = message.content.split(' ')
+                            for i in messageWords:
+                                if i == "+duration":
+                                    time = messageWords[messageWords.index(i) + 1]
+                            time = time.split(":")
+                            hours = int(time[0])
+                            minutes = int(time[1])
+                            endTime = str(datetime.now() + timedelta(hours = hours, minutes = minutes))[0:16]
+                            print(pollMessage.id)
+                            self.database.createPoll(int(pollMessage.id), endTime)
+
+                            timeSeconds = int(time[0]) * 60 * 60 + int(time[1]) * 60
+                            await asyncio.sleep(timeSeconds)
+                            pollMessage = await pollMessage.channel.get_message(pollMessage.id)
+                            reactions = []
+                            for reaction in pollMessage.reactions:
+                                async for user in reaction.users():
+                                    if user == self.bot.user:
+                                        reactions.append(reaction.count - 1)
+                            j = 0
+                            for i in range(len(reactions)):
+                                j+=reactions[i]
+
+                            if not j==0: #if only the ot has reactions, nothing gets sent
+                                plt.subplots(figsize(9,6))
+                                plt.bar(final_options, reactions, width = 0.8, bottom = 0)
+                                plt.title(title, fontsize=27)
+                                plt.savefig('results.png')
+                                print(reactions)
+                                print(final_options)
+                                await message.channel.send('Results for a passed poll created by: <@' + str(message.author.id) + ">", file=discord.File('results.png'))
 
                     except KeyError:
                         return "Please make sure you are using the format 'poll: {title} [Option1] [Option2] [Option 3]'"
             else:
                 return
 
+    def form(self, pct, allvals):
+        absolute = int(pct / 100. * np.sum(allvals))
+        return "{:.1f}%\n({:d})".format(pct, absolute)
 
 def setup(bot):
     bot.add_cog(Poll(bot))
