233b08fab262a4c6de5010cf1177e045e7895dc5
Agent-Fennec <12753219+Agent-Fennec@users.noreply.github.com>
2019-05-27 20:17:57 -0400

    formating

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,14 +1,13 @@
-#thanks to evieepy on github https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
+# thanks to evieepy on github https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
 import discord
 from discord.ext import commands
 
 
 class Owner(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name = "load", hidden = True)
+    @commands.command(name="load", hidden=True)
     @commands.is_owner()
     async def load(self, ctx, *, cog: str):
         try:
@@ -18,27 +17,28 @@ class Owner(commands.Cog):
         else:
             await ctx.send("**`SUCCESS`**")
 
-    @commands.command(name = "unload", hidden = True)
+    @commands.command(name="unload", hidden=True)
     @commands.is_owner()
     async def unload(self, ctx, *, cog: str):
         print(cog)
         try:
             self.bot.unload_extension(cog)
         except Exception as e:
-            await ctx.send('**`ERROR:`**')
+            await ctx.send("**`ERROR:`**")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
 
-    @commands.command(name='reload', hidden=True)
+    @commands.command(name="reload", hidden=True)
     @commands.is_owner()
     async def reload(self, ctx, *, cog: str):
         try:
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send('**`ERROR:`**')
+            await ctx.send("**`ERROR:`**")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
+
 
 def setup(bot):
     bot.add_cog(Owner(bot))
