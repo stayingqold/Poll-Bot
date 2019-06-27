c3d755ceb64d6506d3d57624224fc33e4d2c74ed
finn <finnr@protonmail.com>
2019-06-27 15:56:07 -0700

    now posts stats to discordbots.org again

--- cogs/stats.py
+++ cogs/stats.py
@@ -4,34 +4,23 @@ import config
 import asyncio
 import aiohttp
 import config
+import dbl
 
 
 class Stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.dblpy = dbl.Client(self.bot, config.orgtoken)
 
-    async def send_stats(self):
-        tokens = (
-            ("https://discordbots.org/api/bots/%s/stats", config.orgtoken),
-            ("https://bots.discord.pw/api/bots/%s/stats", config.pwtoken),
-            ("https://botsfordiscord.com/api/v1/bots/%s", config.botsfordiscordtoken),
-        )
-
-        payload = {
-            "Content-Type": "application/json",
-            "server_count": len(self.bot.guilds),
-        }
-        for url, token in tokens:
-            headers = {"Authorization": token}
-            await self.bot.http_session.post(
-                url % self.bot.user.id, json=payload, headers=headers
-            )
-
+    @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        await self.send_stats()
+        await self.dblpy.post_guild_count()
+        print("Guild joined")
 
+    @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        await self.send_stats()
+        await self.dblpy.post_guild_count()
+        print("Guild removed")
 
 
 def setup(bot):
