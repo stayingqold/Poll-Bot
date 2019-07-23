315944ed7790f6b8099cba966fe393003faa60ba
stayingqold <finnr@protonmail.com>
2019-07-23 12:24:56 -0700

    added partner: Deployments.pro

--- cogs/meta.py
+++ cogs/meta.py
@@ -9,7 +9,7 @@ class Meta(commands.Cog):
     @commands.command(name="help")
     async def help(self, ctx):
         emb1 = discord.Embed(
-            description="**Reaction Poll**\nCreate a reaction poll by typing `+poll *your message*`. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\nCreate a reaction poll with multiple options by typing `+poll {title} [Option1] [Option2] [Option3]`.\n\n**Strawpoll**\nCreate a strawpoll by typing `+strawpoll {title} [Option1] [Option2] [Option 3]`, with up to 30 options.\n\n**Other Commands**\n+invite, +donate\n\n**Still Have Questions?**\nJoin our Discord server: <https://discord.gg/FhT6nUn>"
+            description="**Reaction Poll**\nCreate a reaction poll by typing `+poll *your message*`. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\nCreate a reaction poll with multiple options by typing `+poll {title} [Option1] [Option2] [Option3]`.\n\n**Strawpoll**\nCreate a strawpoll by typing `+strawpoll {title} [Option1] [Option2] [Option 3]`, with up to 30 options.\n\n**Other Commands**\n+invite, +donate, +partners\n\n**Still Have Questions?**\nJoin our Discord server: <https://discord.gg/FhT6nUn>"
             + "\n"
             + "Ask us on Twitter: <https://twitter.com/DiscordPollBot>",
             colour=0x83BAE3,
@@ -29,6 +29,17 @@ class Meta(commands.Cog):
             colour=0x83BAE3,
         )
         await ctx.message.channel.send(embed=emb1)
+
+    @commands.command(name="partners")
+    async def partners(self, ctx):
+        embed = discord.Embed(colour=discord.Colour(0x83bae3))
+
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/603292716691881988/603302880115687445/image0.jpg")
+        embed.set_author(name="Poll Bot Partners", icon_url="https://pbs.twimg.com/profile_images/946629893458034688/os3gCiru_400x400.jpg")
+
+        embed.add_field(name="Deployments.pro", value="We’re Deployments.pro! We offer inexpensive hosting services! Great uptime, lower cost. What have you got to lose?")
+        embed.add_field(name="Links", value="Website: https://deployments.pro\nDiscord: https://discord.gg/7aVwdCN\nTwitter: https://twitter.com/deploymentsp")
+        await ctx.message.channel.send(embed=embed)
 
     @commands.command(name="invite")
     async def invite(self, ctx):
