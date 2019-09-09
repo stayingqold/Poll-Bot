fe1d557753aa4bb6ed42632a1d5e45eb1b6a7b87
stayingqold <finnr@protonmail.com>
2019-09-08 17:48:43 -0700

    added config.json, reworked the way stats are posted to discordbots.org

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import logging
-import config
 import aiohttp
 
 logging.basicConfig(level=logging.INFO)
@@ -10,19 +9,27 @@ extensions = (
     "cogs.poll", 
     "cogs.strawpoll", 
     "cogs.meta", 
-    "cogs.stats", 
-    "cogs.owner"
+    "cogs.owner",
+    "cogs.BackgroundTasks"
     )
 
 class PollBot(commands.AutoShardedBot):
-    def __init__(self, num_shards, shard_ids):
+    def __init__(self, config):
         prefixes = ["+", "poll:", "Poll:", "POLL:"]
         super().__init__(
             command_prefix = prefixes,
             status = discord.Status.online,
             activity = discord.Game(name = "+help"))
-        self.shard_count = num_shards
-        self.shard_ids = shard_ids
+        self.config = config
+
+        self.shard_count = self.config["shards"]["count"]
+        shard_ids_list = []
+        shard_ids = []
+        # create list of shard ids
+        for i in range(self.config["shards"]["first_shard_id"], self.config["shards"]["last_shard_id"]+1):
+            shard_ids_list.append(i)
+        self.shard_ids = tuple(shard_ids_list)
+
         self.remove_command("help")
         
         for extension in extensions:
@@ -40,4 +47,4 @@ class PollBot(commands.AutoShardedBot):
             await self.process_commands(message)
 
     def run(self):
-        super().run(config.discordToken, reconnect=True)
+        super().run(self.config["discord_token"], reconnect=True)
--- launcher.py
+++ launcher.py
@@ -3,26 +3,10 @@
 import discord
 from bot import PollBot
 import sys
-try:
-	num_shards = int(sys.argv[1])
+import json
 
-	# In the format first_id:last_id
-	shard_id_range = sys.argv[2]
-	beginning = int(shard_id_range[0:shard_id_range.find(":")])
-	end = int(shard_id_range[shard_id_range.find(":")+1:len(shard_id_range)])
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
-	shard_ids_list = []
-	shard_ids = []
-	# create list of shard ids
-	for i in range(beginning, end+1):
-		shard_ids_list.append(i)
-
-	# convert list to tuple
-	shard_ids = tuple(shard_ids_list)
-
-except:
-	print("Add arguments for number of shards and the shard ID range\nEx: python3 launcher.py 10 0:9")
-	exit()
-
-bot = PollBot(num_shards, shard_ids)
+bot = PollBot(config)
 bot.run()
