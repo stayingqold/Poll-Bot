e8aae3d9dafd3f456174862bab2a2e8f76e59bcb
stayingqold <finnr@protonmail.com>
2020-01-11 11:23:08 -0800

    no longer requires dev to calculate number of shards, no longer uses a database to update server count on discordbots.org

--- cogs/BackgroundTasks.py
+++ cogs/BackgroundTasks.py
@@ -1,26 +1,20 @@
 import discord
 from discord.ext import commands
 import asyncio
-from cogs.Database import Database
 
 class BackgroundTasks(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.db = Database(self.bot.config)
         self.bg_task = self.bot.loop.create_task(self.update_server_count())
 
     @commands.Cog.listener(name='on_ready')
     async def update_server_count(self):
         while not self.bot.is_closed():
-            await self.db.post_server_stats(self.bot.config["physical_server_name"], len(self.bot.guilds))
-            # This sleep makes sure both servers post their stats before we try to retrieve them
-            await asyncio.sleep(30)
-            num_guilds = await self.db.get_server_stats()
             headers = {"Authorization": self.bot.config["discordbotsorg_token"]}
             payload = {
                 "Content-Type": "application/json",
-                "server_count": num_guilds,
+                "server_count": len(self.bot.guilds),
             }
             await self.bot.http_session.post(
                     f"https://discordbots.org/api/bots/298673420181438465/stats", json=payload, headers=headers
